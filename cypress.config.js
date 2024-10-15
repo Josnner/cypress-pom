0b21bd9ce0616082a8c820d8070c4164a738ee5c
Josnner <josnner@gmail.com>
2024-10-15 11:27:41 +0200

    Feature terminado

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,23 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',  // Reporter para Mochawesome
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);  // Plugin para Mochawesome
+    },
     specPattern: [
       'cypress/e2e/**/*.cy.js',        // Archivos en cypress/e2e
       'cypress/integration/**/*.js',   // Archivos en cypress/integration
     ],
   },
-  chromeWebSecurity: false,           // Desactiva chromeWebSecurity
-  experimentalStudio: true,           // Activa experimentalStudio
-  $schema: "https://on.cypress.io/cypress.schema.json" // Esquema de referencia
+  chromeWebSecurity: false,           // Desactiva chromeWebSecurity para evitar problemas con CORS
+  experimentalStudio: true,           // Activa el modo experimentalStudio
 });
